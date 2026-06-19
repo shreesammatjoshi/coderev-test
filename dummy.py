fc4e6e3785e890ee260c3f90d4fbb564b6625602
shreesammatjoshi <shreesammat.joshi@accenture.com>
2026-06-19 10:39:16 +0530

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,20 @@
+password = "secret123"
+
+def login(inp):
+    eval(inp)
+
+def get_data(id):
+    query = "SELECT * FROM users WHERE id=" + id
+    return query
+
+def calc(a,b)
+    return a+b
+
+def ugly( a,b ):
+  x=1
+  y=2
+  if a==True:
+      if b==True:
+          print("bad code")
+
+os.system("rm -rf /")
